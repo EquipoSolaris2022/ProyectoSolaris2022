10bff1ad4ca97bc22d2440a62a7a1b874db9e28a
AndresTapia18 <111100886+AndresTapia18@users.noreply.github.com>
2022-08-21 11:32:56 -0500

    Add files via upload
    Subida de la clase empresa

---
+++ src/main/java/edu/udea/Proyecto_solaris/Empresa.java
@@ -0,0 +1,44 @@
+public class Empresa {
+    private String nombre;
+    private String direccion;
+    private int telefono;
+    private int NIT;
+
+    public Empresa(String nombre, String direccion, int telefono, int NIT) {
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.NIT = NIT;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public int getNIT() {
+        return NIT;
+    }
+
+    public void setNIT(int NIT) {
+        this.NIT = NIT;
+    }
+}
