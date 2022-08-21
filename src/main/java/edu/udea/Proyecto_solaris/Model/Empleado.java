3b829b836212beca5eca0e6f18d61fee2d418bfa
gegitrepo <110982901+gegitrepo@users.noreply.github.com>
2022-08-21 12:21:09 -0500

    Update Empleado.java
    clase empleado update

--- src/main/java/edu/udea/Proyecto_solaris/Model/Empleado.java
+++ src/main/java/edu/udea/Proyecto_solaris/Model/Empleado.java
@@ -1,4 +1,24 @@
 package edu.udea.Proyecto_solaris.Model;
 
 public class Empleado {
-}
+    private String nombre;
+    private String correo;
+    private String empresa;
+    private String rol;
+    public Empleado(String nombre, String correo, String empresa, String rol) {
+        this.nombre = nombre;
+        this.correo = correo;
+        this.empresa = empresa;
+        this.rol = rol;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
