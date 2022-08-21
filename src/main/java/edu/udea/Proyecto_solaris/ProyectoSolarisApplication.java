92f9fad122b6a86c158be583851a0d2ea23fde9a
HaroldZarate <110990881+HaroldZarate@users.noreply.github.com>
2022-08-21 11:18:40 -0500

    Inicializando proyecto equipo solaris

---
+++ src/main/java/edu/udea/Proyecto_solaris/ProyectoSolarisApplication.java
@@ -0,0 +1,13 @@
+package edu.udea.Proyecto_solaris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoSolarisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoSolarisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/udea/Proyecto_solaris/ProyectoSolarisApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.udea.Proyecto_solaris;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoSolarisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
